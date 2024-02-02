e128de3b77729c7055b6b56cc7b34854f400bd75
Malcolm Abbott <mjacoding@gmail.com>
2024-02-02 02:07:51 +0000

    Completed array-manipulation-1 exercise Malcolm Abbott

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,10 @@
+'use strict';
+/* exported head */
+/*
+PSEUDOCODE
+Define a function named head that accepts one parameter: array, of an array | list data type
+Return the the array at 0 index
+*/
+function head(array) {
+  return array[0];
+}
