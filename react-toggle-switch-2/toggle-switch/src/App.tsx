3d4be77b6f6dffae23a48bf955cd787525d70d00
Malcolm Abbott <mjacoding@gmail.com>
2024-05-24 00:18:00 +0000

    Finished with tailwind

--- react-toggle-switch-2/toggle-switch/src/App.tsx
+++ react-toggle-switch-2/toggle-switch/src/App.tsx
@@ -4,15 +4,14 @@ import { Switch } from './Components/Switch';
 function App() {
   const [isOn, setIsOn] = useState(true);
 
-  // function clickHandler():void {
-  //   setIsOn(!isOn);
-  //   console.log(isOn);
-  // }
-
   return (
     <div className="flex h-screen">
       <div className="m-auto">
-        <Switch onClick={() => setIsOn(!isOn)} status={isOn ? 'ON' : 'OFF'} />
+        <Switch
+          onClick={() => setIsOn(!isOn)}
+          status={isOn ? 'ON' : 'OFF'}
+          side={isOn ? 'left-0' : 'right-0'}
+        />
       </div>
     </div>
   );
--- react-toggle-switch-2/toggle-switch/src/Components/Switch.tsx
+++ react-toggle-switch-2/toggle-switch/src/Components/Switch.tsx
@@ -1,15 +1,17 @@
 type Props = {
   status: string;
   onClick: () => void;
+  side: string;
 };
 
-export function Switch({ status, onClick }: Props) {
+export function Switch({ status, onClick, side }: Props) {
   return (
     <div className="flex">
       <div
-        className="h-12 w-32 bg-lime-500 rounded-3xl ring-2 ring-black cursor-pointer"
+        className="h-12 w-32 bg-lime-500 rounded-3xl ring-2 ring-black cursor-pointer relative shadow-xl"
         onClick={onClick}>
-        <div className="h-full w-3/6 bg-white rounded-3xl ring-2 ring-black"></div>
+        <div
+          className={`h-full w-3/6 bg-white rounded-3xl ring-2 ring-black absolute ${side}`}></div>
       </div>
       <h1 className="grid place-items-center pl-8 font-bold text-lg">
         {status}
