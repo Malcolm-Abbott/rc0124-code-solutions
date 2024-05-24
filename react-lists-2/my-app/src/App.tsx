bea90c33485d271496b0bdc47ce6eb8a5523aa09
Malcolm Abbott <mjacoding@gmail.com>
2024-05-24 00:44:34 +0000

    Finished with tailwind

---
+++ react-lists-2/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import { List } from './Components/List';
+
+function App() {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <List />
+      </div>
+    </div>
+  );
+}
+
+export default App;
