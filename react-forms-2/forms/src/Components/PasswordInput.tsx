e887f75ebfc8e245969e8218ddca7d22a1c59cf8
Malcolm Abbott <mjacoding@gmail.com>
2024-05-28 00:32:59 +0000

    Finished with tailwind

---
+++ react-forms-2/forms/src/Components/PasswordInput.tsx
@@ -0,0 +1,11 @@
+export function PasswordInput() {
+  return (
+    <label className="font-semibold">
+      <div>Password</div>
+      <input
+        type="password"
+        name="password"
+        className="ring-2 ring-slate-300 rounded-md px-2"></input>
+    </label>
+  );
+}
---
+++ react-forms-2/forms/src/Components/UsernameInput.tsx
@@ -0,0 +1,11 @@
+export function UsernameInput() {
+  return (
+    <label className="font-semibold">
+      <div>Username</div>
+      <input
+        type="text"
+        name="username"
+        className="ring-2 ring-slate-300 rounded-md px-2"></input>
+    </label>
+  );
+}
