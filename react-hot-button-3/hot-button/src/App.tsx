d320a69dd92e646d16909f453e66ed4e8b875302
Malcolm Abbott <mjacoding@gmail.com>
2024-06-19 23:33:58 +0000

    Completed in tailwind

---
+++ react-hot-button-3/hot-button/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './Components/Button';
+
+function App() {
+  return (
+    <>
+      <div className="flex h-screen">
+        <div className="m-auto">
+          <Button />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
