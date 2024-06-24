6f118a8c714171d34020d5b5e947347357daa99a
Malcolm Abbott <mjacoding@gmail.com>
2024-06-24 18:36:43 +0000

    Finished with tailwind

--- react-multiple-components-3/my-app/src/Components/Numbers.tsx
+++ react-multiple-components-3/my-app/src/Components/Numbers.tsx
@@ -1,14 +1,28 @@
 type Props = {
   items: string[];
+  setIndex: (arg1: number) => void;
+  index: number;
 };
 
-export function Numbers({ items }: Props) {
+export function Numbers({ items, setIndex, index }: Props) {
   return (
     <div className="flex mt-1">
-      {items.map((item, index) => {
+      {items.map((item, i) => {
+        if (index === i)
+          return (
+            <div
+              className="px-8 text-3xl py-4 bg-gradient-to-r from-amber-500 to-amber-700 text-white cursor-pointer hover:bg-gradient-to-l ring-1 ring-amber-900 hover:ring-2 mb-1"
+              key={i}
+              onClick={() => setIndex(i)}>
+              {i}
+            </div>
+          );
         return (
-          <div className="px-8 text-3xl py-4 bg-gradient-to-r from-slate-800 to-slate-600 text-white cursor-pointer hover:bg-gradient-to-l ring-1 ring-slate-800 hover:ring-2 mb-1">
-            {index}
+          <div
+            className="px-8 text-3xl py-4 bg-gradient-to-r from-slate-800 to-slate-600 text-white cursor-pointer hover:bg-gradient-to-l ring-1 ring-slate-800 hover:ring-2 mb-1"
+            key={i}
+            onClick={() => setIndex(i)}>
+            {i}
           </div>
         );
       })}
--- react-multiple-components-3/my-app/src/Pages/Home.tsx
+++ react-multiple-components-3/my-app/src/Pages/Home.tsx
@@ -1,9 +1,9 @@
 import { Title } from '../Components/Title';
-// import { useState } from "react";
+import { useState } from 'react';
 import { Button } from '../Components/Button';
 import { Numbers } from '../Components/Numbers';
 
-const items = [
+export const items = [
   'Aardvark',
   'Bengal',
   'Caterpillar',
@@ -13,14 +13,14 @@ const items = [
 ];
 
 export function Home() {
-  // const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
   return (
     <div className="grid place-items-center">
       <Title title={items[index]} />
-      <Button direction="Prev" />
-      <Numbers items={items} />
-      <Button direction="Next" />
+      <Button direction="Prev" index={index} setIndex={setIndex} />
+      <Numbers items={items} setIndex={setIndex} index={index} />
+      <Button direction="Next" index={index} setIndex={setIndex} />
     </div>
   );
 }
