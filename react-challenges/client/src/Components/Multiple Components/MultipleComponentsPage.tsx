52f5a14434d82d6b9ed769eebc0c648859217755
Malcolm Abbott <mjacoding@gmail.com>
2024-07-27 21:26:35 +0000

    Completed MultipleComponentsPage.tsx route

--- react-challenges/client/src/Components/Multiple Components/MultipleComponentsPage.tsx
+++ react-challenges/client/src/Components/Multiple Components/MultipleComponentsPage.tsx
@@ -2,6 +2,7 @@ import { items } from './data';
 import { useState } from 'react';
 import { Title } from './Title';
 import { Button } from './Button';
+import { Numbers } from './Numbers';
 
 export function MultipleComponentsPage() {
   const [index, setIndex] = useState(0);
@@ -10,11 +11,17 @@ export function MultipleComponentsPage() {
     index === 0 ? setIndex(items.length - 1) : setIndex(index - 1);
   }
 
+  function increment() {
+    index === items.length - 1 ? setIndex(0) : setIndex(index + 1);
+  }
+
   return (
     <div className="flex h-screen">
       <div className="m-auto grid place-items-center gap-y-1">
         <Title title={items[index]} />
         <Button text="Prev" onClick={decrement} />
+        <Numbers index={index} setIndex={setIndex} />
+        <Button text="Next" onClick={increment} />
       </div>
     </div>
   );
