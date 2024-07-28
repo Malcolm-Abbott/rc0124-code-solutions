898c1da67252ee40c72515680fb490aea5dfba04
Malcolm Abbott <mjacoding@gmail.com>
2024-07-28 17:52:38 +0000

    Completed SearchPage.tsx route

--- react-challenges/client/src/Components/Search/SearchBar.tsx
+++ react-challenges/client/src/Components/Search/SearchBar.tsx
@@ -1,12 +1,19 @@
 import { FaSearch } from 'react-icons/fa';
 
-export function SearchBar() {
+type Props = {
+  value: string;
+  setValue: (arg1: string) => void;
+};
+
+export function SearchBar({ value, setValue }: Props) {
   return (
     <div className="relative">
       <input
         type="text"
         className="w-96 h-12 border-2 border-black rounded-3xl pl-8 text-slate-500 font-semibold tracking-wider"
         placeholder="search"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
       />
       <FaSearch className="absolute top-1/3 left-2" />
     </div>
--- react-challenges/client/src/Components/Search/SearchPage.tsx
+++ react-challenges/client/src/Components/Search/SearchPage.tsx
@@ -1,10 +1,15 @@
+import { Quotes } from './Quotes';
 import { SearchBar } from './SearchBar';
+import { useState } from 'react';
 
 export function SearchPage() {
+  const [value, setValue] = useState('');
+
   return (
     <div className="flex h-screen">
       <div className="m-auto">
-        <SearchBar />
+        <SearchBar value={value} setValue={setValue} />
+        <Quotes value={value} />
       </div>
     </div>
   );
